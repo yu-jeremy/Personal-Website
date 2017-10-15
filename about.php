ed33667ae84c2af9e6f3a6ef73d0ec77597ae787
Jeremy Yu <jeremy.yu@wustl.edu>
2017-10-15 03:13:02 -0500

    Finished home page and format for about page set up

---
+++ about.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+
+<html lang="en-US">
+<!-- head code from Bootstrap website -->
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>About Me</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+
+    <navbar class="navbar fixed-top navbar-light navbar-expand-sm bg-white">
+      <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="index.php">Home</a>
+          <li class="nav-item active">
+            <a class="nav-link" href="about.php">About Me</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="projects.php">Projects</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="stories.php">Stories</a>
+          </li>
+        </ul>
+      </div>
+    </navbar>
+
+    <div class="text-center container title">
+      <h1>A little about me. Or Yu.</h1>
+    </div>
+    <br>
+    <div id="carouselExampleControls" class="carousel slide container carousel-container" data-ride="carousel">
+      <div class="carousel-inner" role="listbox">
+        <div class="carousel-item active container">
+          <img class="d-block img-fluid" src="assets/vanessa.jpg" alt="First slide">
+        </div>
+        <div class="carousel-item container">
+          <img class="d-block img-fluid" src="assets/elysia.jpg" alt="Second slide">
+        </div>
+        <div class="carousel-item container">
+          <img class="d-block img-fluid" src="assets/sensasians.jpg" alt="Third slide">
+        </div>
+        <div class="carousel-item container">
+          <img class="d-block img-fluid" src="assets/suities.jpg" alt="Fourth slide">
+        </div>
+        <div class="carousel-item container">
+          <img class="d-block img-fluid" src="assets/acf.jpg" alt="Fifth slide">
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+
+    <div class="text-container md-container container">
+      <div class="text-center">
+        <h2 class="text-muted">[Follower]</h2>
+        <p>First and foremost, I am a believer in God, Christ Jesus, and the Gospel</p>
+      </div>
+      <div class="text-center">
+        <h2 class="text-muted">[Brother]</h2>
+        <p>I am a brother, to my sister back at home and to my brothers and sisters in Christ.</p>
+      </div>
+      <div class="text-center">
+        <h2 class="text-muted">[Student]</h2>
+        <p>I am a student.</p>
+      </div>
+      <div class="text-center">
+        <h2 class="text-muted">[Rock Churner]</h2>
+        <p>
+      </div>
+      <div class="text-center">
+        <h2 class="text-muted">[Musician]</h2>
+      </div>
+      <div class="text-center">
+        <h2 class="text-muted">[Sinner]</h2>
+      </div>
+      <div class="text-center">
+        <h2 class="text-muted">[Sports Fanatic]</h2>
+        <p></p>
+      </div>
+      <div class="text-center">
+        <h2 class="text-muted">[Pun Appreciator]</h2>
+        <p></p>
+    </div>
+
+
+
+    <!-- JQuery, JS -->
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+  </body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+
+<html lang="en-US">
+<!-- code between head tags from Bootstrap website -->
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Jeremy Yu</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <!-- W3Schools CSS -->
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <!-- my own CSS file -->
+    <link rel="stylesheet" href="style.css">
+
+  </head>
+  <body>
+
+    <!-- navigation bar -->
+    <navbar class="navbar fixed-top navbar-light navbar-expand-sm bg-white">
+      <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.php">Home</a>
+          <li class="nav-item">
+            <a class="nav-link" href="about.php">About Me</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="projects.php">Projects</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="stories.php">Stories</a>
+          </li>
+        </ul>
+      </div>
+    </navbar>
+
+    <!-- profile picture and home page header -->
+    <div class="text-center container title pic-container pro-pic-fade">
+      <a href="about.php">
+        <img class="img-fluid pro-pic" src="assets/resized_profile.jpg" alt="Responsive img">
+      </a>
+    </div>
+    <br>
+    <div class="text-center container">
+      <h1 style="font-size: 43px;"><strong>Jeremy Yu</strong></h1>
+    </div>
+
+    <!-- contains James 1:2-4 -->
+    <div class="text-center container sm-container">
+      <div class="row justify-content-md-center">
+        <div class="col text-container">
+          <br>
+          <p>Count it all <kbd>joy</kbd>, my brothers, when you meet trials of various kinds,
+          for you know that the testing of your faith produces <kbd>steadfastness</kbd>.
+          And let steadfastness have its full effect, that you may be <kbd>perfect</kbd>
+          and <kbd>complete</kbd>, lacking in nothing.<br><br>
+          <kbd>James 1:2-4</kbd></p>
+        </div>
+      </div>
+    </div>
+
+
+</html>
